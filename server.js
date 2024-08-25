91c501a4e984adb1cd92ac96f700449946aa5984
Ritvik-lab-coder <ritviksharma822@gmail.com>
2024-08-25 23:41:54 +0530

    initial commit

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
+    origin: 'https://jobz-zy-frontend.vercel.app',
     credentials: true,
     methods: ["GET", "PUT", "POST", "PATCH", "DELETE"]
 }));
